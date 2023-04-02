5985a1e482b891ecd8c79d95c2a5b5b70f0a3e79
Christof Dallermassl <christof.dallermassl@43bits.com>
2023-04-02 21:44:05 +0200

    add settings handling

--- js/m32-connect-ui.js
+++ js/m32-connect-ui.js
@@ -2,17 +2,21 @@
 
 const log  = require ('loglevel');
 const { M32CommunicationService, EVENT_M32_CONNECTED, EVENT_M32_DISCONNECTED, EVENT_M32_CONNECT_ERROR } = require('./m32-communication-service');
+const { EVENT_SETTINGS_CHANGED } = require('./m32-storage');
 
 class M32ConnectUI {
-    constructor() {
+    constructor(m32Storage) {
+        this.m32Storage = m32Storage;
+        this.m32Storage.addEventListener(EVENT_SETTINGS_CHANGED, this.settingsChanged.bind(this));
+
         this.connectButton = document.getElementById("connectButton");
         this.voiceOutputCheckbox = document.getElementById("voiceOutputCheckbox");
         this.statusBar = document.getElementById("statusBar");
         this.voiceOutputEnabled = true;
-        this.m32ConnectService = new M32CommunicationService(this.connected);
-        this.m32ConnectService.addEventListener(EVENT_M32_CONNECTED, this.connected);
-        this.m32ConnectService.addEventListener(EVENT_M32_DISCONNECTED, this.disconnected.bind(this));
-        this.m32ConnectService.addEventListener(EVENT_M32_CONNECT_ERROR, this.connectError.bind(this));
+        this.m32CommunicationService = new M32CommunicationService(this.connected);
+        this.m32CommunicationService.addEventListener(EVENT_M32_CONNECTED, this.connected);
+        this.m32CommunicationService.addEventListener(EVENT_M32_DISCONNECTED, this.disconnected.bind(this));
+        this.m32CommunicationService.addEventListener(EVENT_M32_CONNECT_ERROR, this.connectError.bind(this));
 
         this.connectButton.addEventListener('click', this.clickConnect.bind(this), false);
         this.voiceOutputCheckbox.addEventListener('change', this.clickVoiceOutputReceived.bind(this));
@@ -26,15 +30,15 @@ class M32ConnectUI {
 
     //When the connectButton is pressed
     async clickConnect() {
-        if (this.m32ConnectService.isConnected()) {
+        if (this.m32CommunicationService.isConnected()) {
             log.debug("disconnecting")
             //if already connected, disconnect
-            this.m32ConnectService.disconnect();
+            this.m32CommunicationService.disconnect();
 
         } else {
             log.debug("connecting")
             //otherwise connect
-            await this.m32ConnectService.connect();
+            await this.m32CommunicationService.connect();
         }
     }
 
@@ -63,13 +67,20 @@ class M32ConnectUI {
     }
 
     clickVoiceOutputReceived() {
-        log.debug("voice output changed")
+        // saveSettings
+        log.debug("voice output changed", this.m32Storage.settings);
         this.voiceOutputEnabled = this.voiceOutputCheckbox.checked;
-        this.m32ConnectService.enableVoiceOutput(this.voiceOutputEnabled);
-        // FIXME: saveSettings
+        this.m32Storage.settings.voiceOutputEnabled = this.voiceOutputEnabled;
+        this.m32CommunicationService.enableVoiceOutput(this.voiceOutputEnabled);
+        this.m32Storage.saveSettings();
     }
-    
 
+    settingsChanged(settings) {
+        log.debug("settings changed event", settings);
+        this.voiceOutputEnabled = settings.voiceOutputEnabled;
+        this.voiceOutputCheckbox.checked = this.voiceOutputEnabled;
+        this.m32CommunicationService.enableVoiceOutput(this.voiceOutputEnabled);
+    }
 }
 
 module.exports = { M32ConnectUI }
---
+++ js/m32-storage.js
@@ -0,0 +1,72 @@
+'use strict';
+
+const log  = require ('loglevel');
+
+var events = require('events');
+
+const STORAGE_KEY = 'morserino-trainer';
+const STORAGE_KEY_SETTINGS = 'morserino-trainer-settings';
+
+const EVENT_SETTINGS_CHANGED = "settings-changed";
+
+
+class M32Settings {
+    constructor() {
+        this.cwPlayerWpm = 15;
+        this.cwPlayerEws = 0;
+        this.cwPlayerEls = 2;
+        this.qsoRptWords = false;
+        this.voiceOutputEnabled = true;
+    }
+
+    loadFromStoredSettings(storedSettings) {
+        if (storedSettings) {
+    
+            if ('cwPlayerWpm' in storedSettings) {
+                this.cwPlayerWpm = storedSettings.cwPlayerWpm;
+            }
+            if ('cwPlayerEws' in storedSettings) {
+                this.cwPlayerEws = storedSettings.cwPlayerEws;
+            }
+            if ('cwPlayerEls' in storedSettings) {
+                this.cwPlayerEls = storedSettings.cwPlayerEls;
+            }
+            if ('qsoRptWords' in storedSettings) {
+                this.qsoRptWords = storedSettings.qsoRptWords;
+            }
+            if ('voiceOutputEnabled' in storedSettings) {
+                this.voiceOutputEnabled = storedSettings.voiceOutputEnabled;
+            }
+        }
+    }
+}
+
+class M32Storage {
+
+    constructor() {
+        this.settings = new M32Settings();
+        this.eventEmitter = new events.EventEmitter();
+    }
+
+    addEventListener(eventType, callback) {
+        this.eventEmitter.addListener(eventType, callback);
+    }
+
+    loadSettings() {
+        let storedSettings = JSON.parse(localStorage.getItem(STORAGE_KEY_SETTINGS));
+        this.settings.loadFromStoredSettings(storedSettings);
+        this.eventEmitter.emit(EVENT_SETTINGS_CHANGED, this.settings);
+    
+        // setCwPlayerSettings();
+        // setCwSettingsInUIInput();
+        // setCwSettingsInUILabels();
+        // setVoiceOutputEnabledSettings();
+    }
+    
+    saveSettings() {
+        log.debug("save settings", this.settings);
+        localStorage.setItem(STORAGE_KEY_SETTINGS, JSON.stringify(this.settings));
+    }
+}
+
+module.exports = { M32Settings, M32Storage, EVENT_SETTINGS_CHANGED }
