064e40ee7aaf734b22c15c791299f68ffb2d1d0c
Christof Dallermassl <christof.dallermassl@43bits.com>
2023-08-23 16:51:42 +0200

    set version number

--- js/bundle.js
+++ js/bundle.js
@@ -2867,7 +2867,7 @@ const { FileUploadUI } = require('./m32-file-upload-ui');
 // let m32Protocolhandler;
 
 // some constants
-let VERSION = '0.6.1';
+let VERSION = '0.6.2';
 
 const MODE_CW_GENERATOR = 'cw-generator';
 const MODE_ECHO_TRAINER = 'echo-trainer';
