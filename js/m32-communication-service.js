845da0d5006b0a4fd413ac2eb7aca7a8b70d5b00
Christof Dallermassl <christof.dallermassl@43bits.com>
2026-01-26 21:07:12 +0100

    limit sleep to 1 sec after connection

--- js/m32-communication-service.js
+++ js/m32-communication-service.js
@@ -141,7 +141,7 @@ class M32CommunicationService {
             this.reader = this.inputStream.getReader();
 
             log.debug('M32 Communication Service connected to morserino.');
-            await this.sleep(5000); // wait for morserino to settle
+            await this.sleep(1000); // wait for morserino to settle, some morserinos draw too much power right after connection
 
             this.readLoop();
 
--- js/m32main.js
+++ js/m32main.js
@@ -23,7 +23,7 @@ const { FileUploadUI } = require('./m32-file-upload-ui');
 // let m32Protocolhandler;
 
 // some constants
-let VERSION = '0.7.9';
+let VERSION = '0.7.10';
 
 const MODE_CW_GENERATOR = 'cw-generator';
 const MODE_ECHO_TRAINER = 'echo-trainer';
