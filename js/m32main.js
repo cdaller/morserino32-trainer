34bd555c4fb25683ee14daf8505e00e31bd54742
Christof Dallermassl <christof.dallermassl@43bits.com>
2026-02-02 20:31:57 +0100

    reduce connect wait to 3sec

--- js/m32main.js
+++ js/m32main.js
@@ -23,7 +23,7 @@ const { FileUploadUI } = require('./m32-file-upload-ui');
 // let m32Protocolhandler;
 
 // some constants
-let VERSION = '0.7.13';
+let VERSION = '0.7.14';
 
 const MODE_CW_GENERATOR = 'cw-generator';
 const MODE_ECHO_TRAINER = 'echo-trainer';
